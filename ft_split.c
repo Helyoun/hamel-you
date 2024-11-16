2f3247e169e5d9c50c64b2409b79510b5169aa41
Helyoun <hel.youn1337@gmail.com>
2024-11-16 06:33:59 +0100

    ithink i am done

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hamel-yo <hamel-yo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:15:43 by hamel-yo          #+#    #+#             */
-/*   Updated: 2024/11/16 02:12:21 by hamel-yo         ###   ########.fr       */
+/*   Updated: 2024/11/16 06:32:06 by hamel-yo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ static	char	*ft_makestr(char **s, char c)
 	i = 0 ;
 	while ((*s)[i] != c && (*s)[i] != 0)
 		i++;
-	//printf("%d\n",i);
 	str = (char *)ft_calloc(i + 1, sizeof(char));
 	if (str == NULL)
 		return (NULL);
@@ -42,12 +41,21 @@ static	char	*ft_makestr(char **s, char c)
 	while (**s != 0 && **s != c)
 	{
 		str[i] = **s;
-		//printf("%c\n",**s);
 		i++;
 		(*s)++;
 	}
-	//printf(str);
 	return (str);
+}
+
+static	void	*ft_free(char **strs, size_t i)
+{
+	while(i >= 0)
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return (NULL);
 }
 
 char **ft_split(char const *s, char c)
@@ -56,6 +64,8 @@ char **ft_split(char const *s, char c)
 	size_t	i;
 	char	**strs;
 
+	if (s == NULL)
+		return (NULL);
 	w = ft_count_word((char *)s, c);
 	strs = (char **)ft_calloc(w + 1, sizeof(char *));
 	if (strs == NULL)
@@ -66,22 +76,25 @@ char **ft_split(char const *s, char c)
 		if (*s != c)
 		{
 			strs[i] = ft_makestr((char **)&s, c);
+			if (strs[i] == NULL)
+				return (ft_free(strs, i));
 			i++;
 		}
 		s++;
 	}
 	return (strs);
 }
-
+/*
 int main (int c, char **av)
 {
-	char  **str =  ft_split("     hel      lo world", av[1][0]);
+	char  **str =  ft_split(av[1], 'c');
 	int i = 0;
-	for (int i=0 ; str[i];i++)
-		puts(str[i]);
+	printf("%p", str);
+	//for (int i=0 ; str[i];i++)
+	//	puts(str[i]);
 	//while (i <= ft_count_word("hello wold", av[1][0]))
 	//{
 	//	printf("%s\n", str[i]);
 	//	i++;
 	//}
-}
+}*/

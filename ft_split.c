cda82ef097f5e086b09da0c12ff63279f0424f2f
Helyoun <hel.youn1337@gmail.com>
2024-11-16 02:12:39 +0100

    ithink i am done

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hamel-yo <hamel-yo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:15:43 by hamel-yo          #+#    #+#             */
-/*   Updated: 2024/11/15 07:18:14 by hamel-yo         ###   ########.fr       */
+/*   Updated: 2024/11/16 02:12:21 by hamel-yo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,21 @@ static	char	*ft_makestr(char **s, char c)
 	char	*str;
 
 	i = 0 ;
-	while (*s[i] != c && *s[i] != 0)
+	while ((*s)[i] != c && (*s)[i] != 0)
 		i++;
-	str = (char *)ft_calloc(i, sizeof(char ));
+	//printf("%d\n",i);
+	str = (char *)ft_calloc(i + 1, sizeof(char));
 	if (str == NULL)
 		return (NULL);
-
+	i = 0;
+	while (**s != 0 && **s != c)
+	{
+		str[i] = **s;
+		//printf("%c\n",**s);
+		i++;
+		(*s)++;
+	}
+	//printf(str);
 	return (str);
 }
 
@@ -45,27 +54,34 @@ char **ft_split(char const *s, char c)
 {
 	size_t	w;
 	size_t	i;
-	size_t	**strs;
+	char	**strs;
 
-	w = ft_count_word(s, c);
+	w = ft_count_word((char *)s, c);
 	strs = (char **)ft_calloc(w + 1, sizeof(char *));
 	if (strs == NULL)
 		return (NULL);
+	i = 0;
 	while (*s != 0)
 	{
 		if (*s != c)
 		{
-			strs[i] = ft_makestr(&s, c);
-			if (strs[i] == NULL)
-				return(ft_freeall(strs, i));
+			strs[i] = ft_makestr((char **)&s, c);
 			i++;
 		}
 		s++;
 	}
 	return (strs);
 }
-*/
+
 int main (int c, char **av)
 {
-	printf("%s", ft_makestr(&av[1], av[2][0]));
+	char  **str =  ft_split("     hel      lo world", av[1][0]);
+	int i = 0;
+	for (int i=0 ; str[i];i++)
+		puts(str[i]);
+	//while (i <= ft_count_word("hello wold", av[1][0]))
+	//{
+	//	printf("%s\n", str[i]);
+	//	i++;
+	//}
 }

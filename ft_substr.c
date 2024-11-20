ef0e2c7bf1e58fdaf4073d7b8b1e76677dfcb7ed
Helyoun <hel.youn1337@gmail.com>
2024-11-20 09:10:29 +0100

    i think i am done

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hamel-yo <hamel-yo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 07:21:56 by hamel-yo          #+#    #+#             */
-/*   Updated: 2024/11/20 00:30:27 by hamel-yo         ###   ########.fr       */
+/*   Updated: 2024/11/20 09:06:29 by hamel-yo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,51 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	i;
-	char			*str;
+	size_t	ls;
+	char	*str;
 
 	if (s == NULL)
 		return (NULL);
-	if (start >= len)
+	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
-	i = 0;
-	while ((size_t)(i + start) < len && s[i] != 0)
-		i++;
-	str = ft_calloc(i + 1, sizeof (char));
+	ls = len ;
+	if (start + len >= ft_strlen(s))
+		ls = ft_strlen(s);
+	str = ft_calloc(ls + 1, sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	while ((size_t)start != len && s[start] != 0)
+	ls = 0;
+	while (ls < len && s[start + ls] != 0)
 	{
-		str[i] = s[start] ;
-		i++;
-		start++;
+		str[ls] = s[start + ls];
+		ls++;
 	}
 	return (str);
-}
+}	
 /*
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= (unsigned int)ft_strlen(s))
+		return ((char *)ft_calloc(1, sizeof(char)));
+	if ((unsigned int)ft_strlen(s) <= start + len)
+		substr = malloc(sizeof(char) * (ft_strlen(s) - start + 1));
+	else
+		substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	while (s[start] && i < len)
+		substr[i++] = s[start++];
+	substr[i] = '\0';
+	return (substr);
+}
 int main(int ac, char **av)
 {
 	printf("%s", ft_substr(av[1], atoi(av[2]), atoi(av[3])));
